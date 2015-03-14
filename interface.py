1d458da0075c854a949ac90c801ffd513113a357
gamer496 <singhjasdeep496@gmail.com>
2015-03-14 16:48:17 +0530

    all

--- interface.py
+++ interface.py
@@ -1,5 +1,8 @@
 from REST import *
-
+BASE_URL="https://api.github.com"
 username=raw_input("enter the username")
-l=get_user_details(username)
-print [x for x in l]
+details=get_user_details(username,BASE_URL)
+print [x for x in details]
+repo=raw_input()
+collab=get_user_details(username,BASE_URL+"/repos/"+username+"/"+repo+"/collaborators")
+print [x for x in collab]
